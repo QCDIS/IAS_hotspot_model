360b57d85828f2a8fcccbb94f69dcdb8176d04c9
skoulouzis <koulouzis@gmail.com>
2025-06-24 11:21:03 +0200

    description

--- mylifewatch/split_data/main.r
+++ mylifewatch/split_data/main.r
@@ -6,12 +6,17 @@ source("functions/SEanalytics.functions.r")
 
 Speciespath <- "/mnt/inputs/species_to_use"
 Outpath <- "/mnt/outputs/RF.indata"
-
+data_table = "/mnt/inputs/data_table.csv"
 RF_results <- "/mnt/outputs/RF.results.cv"
 
-Species <- read.species(Speciespath) # Read the species list from the input path
 Stack <- file.path(Stackpath, "globalStack.rda") # "globalStack.rda" or "europeStack.rda"
 Iterations.path <- "/mnt/outputs/iterations" # Path to the iterations zip file
 
+Data.table <- read.csv2(data_table,header=TRUE, sep=";",stringsAsFactors = F)
 
-split.data(species = Species, indata.path = Outpath, iterations.path= Iterations.path)
+
+for(Species in Data.table$species){
+    print(Species)
+    species.data <- split.data(species = Species, indata.path = Outpath, iterations.path= Iterations.path)
+    write.csv2(species.data, file = paste(Outpath,"/",Species,"indata.csv", sep=""),row.names=F)
+}
