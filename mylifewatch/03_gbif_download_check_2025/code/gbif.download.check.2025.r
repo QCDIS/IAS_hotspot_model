881d7448a27305f3c86b9a28690b9ba933a3653e
skoulouzis <koulouzis@gmail.com>
2025-10-16 12:08:15 +0200

    skip empty

--- mylifewatch/03_gbif_download_check_2025/code/gbif.download.check.2025.r
+++ mylifewatch/03_gbif_download_check_2025/code/gbif.download.check.2025.r
@@ -210,7 +210,8 @@ for (s in all.species) {
   if (length(cat) < 1) cat <- "no_match"
   print(paste("category:", cat))
   print(paste("filtered.cleanput.marine len:", length(filtered.cleanput.marine)))
-  print(paste("filtered.cleanput.marine species:", filtered.cleanput.marine[filtered.cleanput.marine$species == s]))
+  print(paste("filtered.cleanput.marine:", filtered.cleanput.marine))
+  print(paste("filtered.cleanput.marine species:", filtered.cleanput.marine[filtered.cleanput.marine$species]))
   if (length(which(filtered.cleanput.marine$species == s)) < 1) {
     print(paste("No records for species", s, "skipping to next species"))
     next
