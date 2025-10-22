1d387c6717f4f62f4ebe66c827653bba2cce4250
skoulouzis <koulouzis@gmail.com>
2025-10-22 16:38:34 +0200

    valid_vars

--- mylifewatch/05_invasive_data_masterscript_with_CV_2025/code/invasive_data_masterscript_with_CV_2025.r
+++ mylifewatch/05_invasive_data_masterscript_with_CV_2025/code/invasive_data_masterscript_with_CV_2025.r
@@ -577,8 +577,8 @@ for(species in Data.table$species[-exclude]){
     # subset safely and preserve data.frame structure
     x_var <- my.data[, valid_vars, drop = FALSE]
 
-    print(paste("vars: ", vars))
-    x_var <- my.data[,vars]
+    print(paste("valid_vars: ", valid_vars))
+    x_var <- my.data[,valid_vars]
 
     # skip if all predictor values are NA
     if (all(sapply(x_var, function(col) all(is.na(col))))) {
