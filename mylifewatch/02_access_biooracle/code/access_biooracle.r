0d8bd5a43b7c99cd910476390210ba6dd8107820
skoulouzis <koulouzis@gmail.com>
2025-10-24 13:55:15 +0200

    save to datalayer_nc

--- mylifewatch/02_access_biooracle/code/access_biooracle.r
+++ mylifewatch/02_access_biooracle/code/access_biooracle.r
@@ -19,7 +19,7 @@ inputs_path = "/mnt/inputs/"
 outputs_path <- "/mnt/outputs/"
 
 
-biooracle_datalayer_path <- paste0(outputs_path,"biooracle/datalayer_nc/")
+biooracle_datalayer_path <- paste0(outputs_path,"datalayer_nc/")
 if(!dir.exists(biooracle_datalayer_path)){
   dir.create(biooracle_datalayer_path, recursive = TRUE)
 }
