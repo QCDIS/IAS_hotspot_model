bbe6a20ca171639bced8a0f3d2a992b2d578833e
skoulouzis <koulouzis@gmail.com>
2025-10-24 11:57:42 +0200

    list

--- mylifewatch/run-pipeline.sh
+++ mylifewatch/run-pipeline.sh
@@ -3,7 +3,7 @@
 base_dir="$(pwd)"
 dev_kit_dir="$HOME/workspace/mylifewatch-wrapper-development-kit"
 
-wrapper_paths=("01_gbif_download_2025",
+wrapper_paths=("01_gbif_download_2025"
                "02_access_biooracle"
         )
 
