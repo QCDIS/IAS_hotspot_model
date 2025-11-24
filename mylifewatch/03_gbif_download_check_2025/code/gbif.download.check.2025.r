963cff57e243d1826faed310139cc3e1c2c7e537
skoulouzis <koulouzis@gmail.com>
2025-11-24 16:22:44 +0100

    comment pseudoabsences

--- mylifewatch/03_gbif_download_check_2025/code/gbif.download.check.2025.r
+++ mylifewatch/03_gbif_download_check_2025/code/gbif.download.check.2025.r
@@ -281,36 +281,34 @@ for (my.cathegory in cathegories) {
     }
 
     locationsamples <- sample(1:length(filtered.cleanput.subset$gbifID), n_samples, replace =F)
-    print(paste("locationsamples length:", length(locationsamples)))
-    locationsamples <- sample(seq_len(nrow(filtered.cleanput.subset)), n_samples, replace = FALSE)
-    print(paste("locationsamples length:", length(locationsamples)))
+#     locationsamples <- sample(seq_len(nrow(filtered.cleanput.subset)), n_samples, replace = FALSE)
 
     pseudoabsences <- filtered.cleanput.subset[locationsamples,
                                              c("gbifID", "occurrenceID", "species", "occurrenceStatus", "decimalLongitude", "decimalLatitude",
                                                "coordinateUncertaintyInMeters", "depth", "depthAccuracy", "eventDate")]
 
-    # generate gbifIDs that match the number of rows in pseudoabsences
-    pseudoabsences$gbifID <- paste0("pseudo", seq_len(nrow(pseudoabsences)))
-    pseudoabsences$species <- NA
-    pseudoabsences$occurrenceStatus <- "ABSENT"
-    pseudoabsences$coordinateUncertaintyInMeters <- NA
-    pseudoabsences$depthAccuracy <- NA
-    pseudoabsences$eventDate <- NA
-    names(pseudoabsences) <- c("gbifID", "occurrenceID", "species", "occurrenceStatus", "decimalLongitude", "decimalLatitude",
-                             "coordinateUncertaintyInMeters", "depth", "depthAccuracy", "eventDate")
-    xlim <- c(-180, 180)
-    ylim <- c(-60, 84)
-    test_plot_pseudoabsences = paste(specie_splots_dir, "testplot.pseudoabsences.", my.cathegory, ".jpg", sep = "")
-    print(paste("Plotting test_plot_pseudoabsences: ", test_plot_pseudoabsences))
-    jpeg(test_plot_pseudoabsences,
-       width = 18 * (xlim[2] - xlim[1]), height = 18 * (ylim[2] - ylim[1]), pointsize = 4)
-    plot(world1$geometry, xlim = xlim, ylim = ylim, col = "light grey")
-    lines(boxxlim[c(1, 2, 2, 1, 1)], boxylim[c(1, 1, 2, 2, 1)])
-    points(pseudoabsences$decimalLongitude, pseudoabsences$decimalLatitude, col = "red", pch = "*", cex = 5)
-    dev.off()
-    pseudoabsences_marine_excludebox = paste(speciespath, "pseudoabsences.marine.excludebox", my.cathegory, ".csv", sep = "")
-    print(paste("pseudoabsences_marine_excludebox: ", pseudoabsences_marine_excludebox))
-    write.csv(pseudoabsences, file = pseudoabsences_marine_excludebox, row.names = FALSE)
+#     # generate gbifIDs that match the number of rows in pseudoabsences
+#     pseudoabsences$gbifID <- paste0("pseudo", seq_len(nrow(pseudoabsences)))
+#     pseudoabsences$species <- NA
+#     pseudoabsences$occurrenceStatus <- "ABSENT"
+#     pseudoabsences$coordinateUncertaintyInMeters <- NA
+#     pseudoabsences$depthAccuracy <- NA
+#     pseudoabsences$eventDate <- NA
+#     names(pseudoabsences) <- c("gbifID", "occurrenceID", "species", "occurrenceStatus", "decimalLongitude", "decimalLatitude",
+#                              "coordinateUncertaintyInMeters", "depth", "depthAccuracy", "eventDate")
+#     xlim <- c(-180, 180)
+#     ylim <- c(-60, 84)
+#     test_plot_pseudoabsences = paste(specie_splots_dir, "testplot.pseudoabsences.", my.cathegory, ".jpg", sep = "")
+#     print(paste("Plotting test_plot_pseudoabsences: ", test_plot_pseudoabsences))
+#     jpeg(test_plot_pseudoabsences,
+#        width = 18 * (xlim[2] - xlim[1]), height = 18 * (ylim[2] - ylim[1]), pointsize = 4)
+#     plot(world1$geometry, xlim = xlim, ylim = ylim, col = "light grey")
+#     lines(boxxlim[c(1, 2, 2, 1, 1)], boxylim[c(1, 1, 2, 2, 1)])
+#     points(pseudoabsences$decimalLongitude, pseudoabsences$decimalLatitude, col = "red", pch = "*", cex = 5)
+#     dev.off()
+#     pseudoabsences_marine_excludebox = paste(speciespath, "pseudoabsences.marine.excludebox", my.cathegory, ".csv", sep = "")
+#     print(paste("pseudoabsences_marine_excludebox: ", pseudoabsences_marine_excludebox))
+#     write.csv(pseudoabsences, file = pseudoabsences_marine_excludebox, row.names = FALSE)
 } # --- End category loop ---
 
 
